cf71ab33982ea6cd25b0680853802ee8dd8b6fca
Fritask <fritas.series@gmail.com>
2023-11-21 17:32:23 -0300

    created transactions component

--- expense-control/src/app/app.routes.ts
+++ expense-control/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
+    },
+    {
+        path: 'transactions',
+        component: TransactionsComponent,
     },
 ];
